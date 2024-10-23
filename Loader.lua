ae9b08eb3e3a55ae1d6658391d7cd4932d65f47b
hojixv <153679337+hojixv@users.noreply.github.com>
2024-10-22 20:37:28 -0400

    Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,17 +4,17 @@ if game.PlaceId == 2788229376 or
    game.PlaceId == 7213786345 or 
    game.PlaceId == 16033173781 or 
    game.PlaceId == 16158576873 then 
-    'https://api.luarmor.net/files/v3/loaders/e1db4e340267095ae4ca565ea7f481e3.lua'
+    loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/e1db4e340267095ae4ca565ea7f481e3.lua'))()
 elseif game.PlaceId == 17625359962 then
-    'https://api.luarmor.net/files/v3/loaders/e1esdb38333av332432b32282e2.lua'
+    loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/e1esdb38333av332432b32282e2.lua'))()
 end
 
 getgenv().script_key = script_key
 
 if getgenv().Debug then
-   'https://api.luarmor.net/files/v3/loaders/e1esdb38333av332432b32282e2.lua'
+    loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/e1esdb38333av332432b32282e2.lua'))()
 end
 
 if getgenv().Insets then
-   'https://api.luarmor.net/files/v3/loaders/a1doe933ff333532e22cd21e9.lua
+    loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/a1doe933ff333532e22cd21e9.lua'))()
 end
